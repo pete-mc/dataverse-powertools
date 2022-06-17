3e0c037a1f4059a334897e001d276f5db02f4981
Matt Kemp <matthew.kemp@clade.com.au>
2022-06-17 14:32:36 +1000

    .

--- src/plugins/buildDeployPlugin.ts
+++ src/plugins/buildDeployPlugin.ts
@@ -1,70 +1,5 @@
 import * as vscode from "vscode";
 import * as cp from "child_process";
-<<<<<<< HEAD
-
-export async function buildDeployPlugin(chan: vscode.OutputChannel, context: vscode.ExtensionContext) {
-		vscode.window.showInformationMessage('Building');
-		if (vscode.workspace.workspaceFolders !== undefined) {
-			const workspacePath = vscode.workspace.workspaceFolders[0].uri.fsPath;
-			const connfile = await vscode.workspace.fs.readFile(vscode.Uri.file(workspacePath + "\\connectionstring.txt"));
-			const connString = Buffer.from(connfile).toString('utf8');
-			cp.execFile("dotnet",["build"],{cwd: workspacePath}
-				,(error, stdout) => {
-					if (error) {
-						vscode.window.showErrorMessage("Error building plugins, see output for details.");
-						chan.appendLine(stdout);
-						chan.show();
-					}	else{
-						chan.appendLine(stdout);
-						vscode.window.showInformationMessage('Deploying to Dataverse');
-						cp.execFile(
-							workspacePath + "\\packages\\spkl\\tools\\spkl.exe",
-							[
-								"plugins",
-								"./RWFCore/spkl.json",
-								connString
-							],
-							{
-								cwd: workspacePath
-							}
-							,(error, stdout) => {
-								if (error) {
-									vscode.window.showErrorMessage("Error deploying plugin, see output for details.");
-									chan.appendLine(stdout);
-									chan.show();
-								}	else{
-									chan.appendLine(stdout);
-									vscode.window.showInformationMessage('Plugin Deployed');
-								}
-							});
-					}
-				});
-
-			
-		};		
-};
-
-export async function buildPlugin(chan: vscode.OutputChannel, context: vscode.ExtensionContext) {
-	vscode.window.showInformationMessage('Building');
-	if (vscode.workspace.workspaceFolders !== undefined) {
-		const workspacePath = vscode.workspace.workspaceFolders[0].uri.fsPath;
-		const connfile = await vscode.workspace.fs.readFile(vscode.Uri.file(workspacePath + "\\connectionstring.txt"));
-		const connString = Buffer.from(connfile).toString('utf8');
-		cp.execFile("dotnet",["build"],{cwd: workspacePath}
-			,(error, stdout) => {
-				if (error) {
-					vscode.window.showErrorMessage("Error building plugins, see output for details.");
-					chan.appendLine(stdout);
-					chan.show();
-				}	else{
-					chan.appendLine(stdout);
-				}
-			});
-
-		
-	};		
-};
-=======
 import * as path from "path";
 import * as fs from "fs";
 import DataversePowerToolsContext from "../DataversePowerToolsContext";
@@ -104,4 +39,3 @@ export async function buildDeployPlugin(context: DataversePowerToolsContext) {
         });
     }
 }
->>>>>>> 3295d32d918ba775daf3cce2e386eaf8117634c1
