3d700d0cfb26c57915a192ec111ffece96a1531a
Matt Kemp <matthew.kemp@clade.com.au>
2022-07-19 17:08:13 +1000

    generatetemplate fix

--- src/general/generateTemplate.ts
+++ src/general/generateTemplate.ts
@@ -8,32 +8,43 @@ export async function generateTemplate(context: DataversePowerToolsContext) {
     vscode.window.showInformationMessage("Generating");
     if(context.projectSettings.type && context.projectSettings.templateversion && vscode.workspace.workspaceFolders)
     {
-        var fullFilePath = context.vscode.asAbsolutePath(path.join(context.projectSettings.type));
+        var fullFilePath = context.vscode.asAbsolutePath(path.join("templates", context.projectSettings.type));
         var templates = JSON.parse(fs.readFileSync(fullFilePath + "\\template.json", "utf8")) as Array<PowertoolsTemplate>;
         var templateToCopy = {} as PowertoolsTemplate;
-        templates.every((t)=>{
+        for (const t of templates) {
             if(t.version === context.projectSettings.templateversion)
             {
                 templateToCopy = t;
                 return false;
             }
-        });
-        if(templateToCopy)
+        }
+        vscode.window.showInformationMessage("Generating template version: " + templateToCopy.version.toString());
+        if(templateToCopy && templateToCopy.placeholders)
         {
+            let placeholders = [] as templatePlaceholder[];
+            for (const p of templateToCopy.placeholders) {
+                placeholders.push({placeholder: p.placeholder, value: await vscode.window.showInputBox({
+                    prompt: p.displayName}) as string});
+            }
             templateToCopy.files?.every(async (f)=>{
-                var data = fs.readFileSync(fullFilePath + "\\" + f, "utf8");
-                templateToCopy.placeholders?.every(async (p)=>{
-                    var value = await vscode.window.showInputBox({
-                        prompt: p.displayName,}) as string;
-                    data = data.replace(p.placeholder, value);
-                });
+                var data = fs.readFileSync(fullFilePath + "\\" + f.filename + f.extension + "\\" + context.projectSettings.templateversion + f.extension, "utf8");
+                for (const p of placeholders) {
+                    data = data.replace(new RegExp(p.placeholder, "g"), p.value);
+                }
                 if(vscode.workspace.workspaceFolders){
                     const folderPath = vscode.workspace.workspaceFolders[0].uri.fsPath;
-                    const path = f.path;
-                    path.unshift(folderPath);
-                    await vscode.workspace.fs.writeFile(vscode.Uri.file(path.join(...path)), Buffer.from(data, "utf8"));
+                    const destPath = f.path;
+                    destPath.unshift(folderPath);
+                    destPath.push(f.filename + f.extension);
+                    console.log(vscode.Uri.file(path.join(...destPath)), Buffer.from(data, "utf8"));
+                    await vscode.workspace.fs.writeFile(vscode.Uri.file(path.join(...destPath)), Buffer.from(data, "utf8"));
                 }
             });
         }
     }
+}
+
+interface templatePlaceholder {
+    placeholder: string;
+    value: string;
 }
