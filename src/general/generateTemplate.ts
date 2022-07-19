ff7203d760fc272c29841d6ef8d263a198a679ac
Matt Kemp <matthew.kemp@clade.com.au>
2022-07-19 15:33:43 +1000

    generatetemplate

--- src/general/generateTemplate.ts
+++ src/general/generateTemplate.ts
@@ -1,20 +1,39 @@
 import * as vscode from 'vscode';
 import * as cp from "child_process";
-import DataversePowerToolsContext, { ProjectTypes } from "../DataversePowerToolsContext";
+import DataversePowerToolsContext, { PowertoolsTemplate, ProjectTypes } from "../DataversePowerToolsContext";
+import path = require("path");
+import fs = require("fs");
 
-export async function generatTemplate(context: DataversePowerToolsContext) {
+export async function generateTemplate(context: DataversePowerToolsContext) {
     vscode.window.showInformationMessage("Generating");
-    if (vscode.workspace.workspaceFolders !== undefined) {
-        const workspacePath = vscode.workspace.workspaceFolders[0].uri.fsPath;
-        cp.execFile("dotnet", ["new", "console"], { cwd: workspacePath }, (error, stdout) => {
-            if (error) {
-                vscode.window.showErrorMessage("Error generating template, see output for details.");
-                context.channel.appendLine(stdout);
-                context.channel.show();
-            } else {
-                context.channel.appendLine(stdout);
-                vscode.window.showInformationMessage("Generated");
+    if(context.projectSettings.type && context.projectSettings.templateversion && vscode.workspace.workspaceFolders)
+    {
+        var fullFilePath = context.vscode.asAbsolutePath(path.join(context.projectSettings.type));
+        var templates = JSON.parse(fs.readFileSync(fullFilePath + "\\template.json", "utf8")) as Array<PowertoolsTemplate>;
+        var templateToCopy = {} as PowertoolsTemplate;
+        templates.every((t)=>{
+            if(t.version === context.projectSettings.templateversion)
+            {
+                templateToCopy = t;
+                return false;
             }
         });
+        if(templateToCopy)
+        {
+            templateToCopy.files?.every(async (f)=>{
+                var data = fs.readFileSync(fullFilePath + "\\" + f, "utf8");
+                templateToCopy.placeholders?.every(async (p)=>{
+                    var value = await vscode.window.showInputBox({
+                        prompt: p.displayName,}) as string;
+                    data = data.replace(p.placeholder, value);
+                });
+                if(vscode.workspace.workspaceFolders){
+                    const folderPath = vscode.workspace.workspaceFolders[0].uri.fsPath;
+                    const path = f.path;
+                    path.unshift(folderPath);
+                    await vscode.workspace.fs.writeFile(vscode.Uri.file(path.join(...path)), Buffer.from(data, "utf8"));
+                }
+            });
+        }
     }
 }
