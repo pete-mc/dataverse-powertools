5423bef349b477812d067e6fc46dd25c7dbe23b4
Peter McDonald <peter.mcdonald@clade.com.au>
2022-07-20 11:40:00 +1000

    changed to for loop

--- src/general/restoreDependencies.ts
+++ src/general/restoreDependencies.ts
@@ -7,21 +7,20 @@ export async function restoreDependencies(context: DataversePowerToolsContext) {
 		const util = require('util');
 		const execFile = util.promisify(cp.execFile);
 		vscode.window.showInformationMessage('Restoring dependencies...');
-		if (vscode.workspace.workspaceFolders !== undefined && context.template !== undefined) {
+		if (vscode.workspace.workspaceFolders !== undefined && context.template !== undefined && context.template.restoreCommands) {
 			const workspacePath = vscode.workspace.workspaceFolders[0].uri.fsPath;
-			const template = context.template;
-			template.restoreCommands?.every(async (c)=>{
-				const {error, stdout} = await execFile(c.command, c.params, { cwd: workspacePath,  });
+			for (const c of context.template.restoreCommands) {
+					const {error, stdout} = await execFile(c.command, c.params, { cwd: workspacePath,  });
     		if (error) {
 						vscode.window.showErrorMessage("Error running " + c.command + " " + c.params.join(" "));
 						context.channel.appendLine(stdout);
 						context.channel.show();
-						return false;
+						break;
 					}
 					else{
 						context.channel.appendLine(stdout);
 					}
-			});
+			};
 			vscode.window.showInformationMessage('Restore Complete.');
 		};		
 };
